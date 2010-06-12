03cc2ffdf4cc947828807c2477343b3b90ddd57c
kodavali.satish <kodavali.satish@d76c474b-ed4b-0410-a7b2-0db244337b31>
2010-06-12 07:40:26 +0000

    Added the Ans,NumSum,DenomSum and IsMultiple types for fraction addition tutoring in xPST

---
+++ xPSTLib/src/org/clearsighted/tutorengine/types/IsMultiple.java
@@ -0,0 +1,79 @@
+package org.clearsighted.tutorengine.types;
+
+import java.util.HashMap;
+
+import org.clearsighted.tutorengine.GoalNode;
+import org.clearsighted.tutorengine.Type;
+
+public class IsMultiple extends Type {
+	
+	Double myval = null;
+	String s1;
+
+	@Override
+	public boolean check(HashMap<String, GoalNode> gns,String value)
+	{
+		if (myval == null)
+			return false;
+		double si;
+		try
+		{
+			si = Double.parseDouble(value);
+		}
+		catch (NumberFormatException e)
+		{
+			return false;
+		}
+		myval = Double.parseDouble(gns.get(s1).getProperty("answer").toString());
+		return isMultiple(myval,si);
+	}
+	
+	public boolean isMultiple(double a,double b)
+	{
+		if(b%a == 0)
+			return true;
+		else
+			return false;
+	}
+
+	@Override
+	public void construct(Object[] args)
+	{
+		s1 = (String)args[0];
+		myval = 0.0;
+	}
+
+	@Override
+	public Object getRepresentativeValue()
+	{
+		return myval;
+	}
+
+	@Override
+	public String toString()
+	{
+		return myval.toString();
+	}
+	
+	public boolean compare(HashMap<String, GoalNode> gns,String value,String op)
+	{
+		if (myval == null)
+			return false;
+		double si;
+		try
+		{
+			si = Double.parseDouble(value);
+		}
+		catch (NumberFormatException e)
+		{
+			return false;
+		}
+		myval = Double.parseDouble(gns.get(s1).getProperty("answer").toString());
+		
+		if(op.equals("e"))
+			return isMultiple(myval,si);
+		if(op.equals("ne"))
+			return !isMultiple(myval,si);
+		return false;
+	}
+}
